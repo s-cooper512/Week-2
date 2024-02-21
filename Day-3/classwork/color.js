2cd5a770a3b22a043b98f9c89f801338e7a6632d
Stephen Cooper <scooper@indeed.com>
2024-02-21 16:09:19 -0600

    Adds Day 3 classwork (color.html and color.js)

---
+++ Day-3/classwork/color.js
@@ -0,0 +1,11 @@
+let color = "pretty color goes here";
+
+if (color === "green" || color === "Green") {
+    console.log("GO");
+} else if (color === "yellow" || color === "Yellow") {
+    console.log("SLOW");
+} else if (color === "red" || color === "Red") {
+    console.log("STOP");
+} else {
+    console.log("CONFUSED");
+}
