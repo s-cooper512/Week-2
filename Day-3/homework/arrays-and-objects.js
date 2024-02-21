8234f31290356414891b8771322118fe440ca6f4
Stephen Cooper <scooper@indeed.com>
2024-02-21 15:47:38 -0600

    Added comments to bonus

--- Day-3/homework/arrays-and-objects.js
+++ Day-3/homework/arrays-and-objects.js
@@ -157,13 +157,13 @@ user.address = {
 
 console.log("Address: " + user.address.city);
 //8.7
-for (i=0;Object.keys(user).length > i;i++) {
-    let category = Object.keys(user)[i];
+for (i=0;Object.keys(user).length > i;i++) { //for each variable in user
+    let category = Object.keys(user)[i]; //assign name of variable (not content) to category
     if (category !== "address") {
-        console.log(category + ": " + user[category]);
+        console.log(category + ": " + user[category]); //output value of given variable in users
      } else if (category == "address") {
         console.log(category + ": ");
-        for (j=0; Object.keys(user.address).length > j;j++) {
+        for (j=0; Object.keys(user.address).length > j;j++) { //same as parent loop, but only for variables under the address object
             category = Object.keys(user.address)[j];
             console.log(category + ": " + user.address[category]);
         }
